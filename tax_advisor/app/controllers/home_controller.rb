c34e4c680d420bf5c30f1f27df4fe98c40199b7c
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2013-02-25 22:50:53 +0530

    first commit

---
+++ tax_advisor/app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def about_us
+    
+  end
+  
+  def our_clients
+    
+  end
+  
+  def tax_guide
+    
+  end
+ 
+  def services
+    
+  end
+ 
+  def contacts
+    
+  end
+   
+end
---
+++ tax_advisor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaxAdvisor::Application.config.secret_token = 'c43ab72e12817ef4c55958ff11f61dfeb47451cbb9ef4fe13533a66fca6178bca6ef54e37fb176427ccbfbca1c4e746fca60a7234e5c41f08f98001b1912f95e'
